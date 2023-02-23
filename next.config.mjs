e02419a546b756c07a6826c9796423e0bd6c93ea
zaflaton <59338890+zaflaton@users.noreply.github.com>
2023-02-23 21:13:26 +0800

    final commit

--- next.config.mjs
+++ next.config.mjs
@@ -20,5 +20,13 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 export default config;
